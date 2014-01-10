78338c8b1d6d6244943ea7312107e24c33866ec4
Denis Rizov <d.b.rizov@gmail.com>
2014-01-10 16:17:31 +0200

    Conditional Statements added

---
+++ src/conditional/statements/GreatestOfFiveNumbers.java
@@ -0,0 +1,22 @@
+package conditional.statements;
+
+public class GreatestOfFiveNumbers {
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 5, 2, 4, 3 };
+		
+		System.out.println(findGreatestNumber(numbers));
+	}
+	
+	private static int findGreatestNumber(int[] numbers) {
+		int greatest = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < numbers.length; i++) {
+			if (greatest < numbers[i]) {
+				greatest = numbers[i];
+			}
+		}
+		
+		return greatest;
+	}
+}
